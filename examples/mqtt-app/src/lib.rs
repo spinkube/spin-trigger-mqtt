1f87b08a40af79c8669112ef5a326aaaa3024307
Brian H <brian.hardock@fermyon.com>
2024-03-12 14:01:15 -0600

    feat(sdk): add sdk macro
    Co-Authored-By: Karthik Ganeshram <karthik.ganeshram@fermyon.com>
    Signed-off-by: Brian H <brian.hardock@fermyon.com>

--- examples/mqtt-app/src/lib.rs
+++ examples/mqtt-app/src/lib.rs
@@ -1,24 +1,15 @@
+use spin_mqtt_sdk::{mqtt_component, Payload};
 use chrono::{DateTime, Utc};
 
-wit_bindgen::generate!({
-    world: "spin-mqtt",
-    path: "../../",
-    exports: {
-        world: SpinMqtt
-    }
-});
+#[mqtt_component]
+fn handle_message(message: Payload) -> anyhow::Result<()> {
+    let datetime: DateTime<Utc> = std::time::SystemTime::now().into();
+    let formatted_time = datetime.format("%Y-%m-%d %H:%M:%S.%f").to_string();
 
-struct SpinMqtt;
-
-impl Guest for SpinMqtt {    
-    fn handle_message(message: Vec<u8>) {               
-        let datetime: DateTime<Utc> = std::time::SystemTime::now().into();
-        let formatted_time = datetime.format("%Y-%m-%d %H:%M:%S.%f").to_string();
-
-        println!(
-            "{:?} Message received by wasm component: '{}'",
-            formatted_time,
-            String::from_utf8_lossy(&message)
-        );
-    }
+    println!(
+        "{:?} Message received by wasm component: '{}'",
+        formatted_time,
+        String::from_utf8_lossy(&message)
+    );
+    Ok(())
 }
